14bcb6a6eff16948d2e76643fe67b83d15e73d4f
Bruno Cochofel <bruno.cochofel@gmail.com>
2020-02-03 15:56:40 +0000

    i3 focus with arrows

--- .config/i3/scripts/i3-cheat-sheet.sh
+++ .config/i3/scripts/i3-cheat-sheet.sh
@@ -7,8 +7,8 @@ export TEXT_FILE="${HOME}/.config/i3/scripts/i3-cheat-sheet.txt"
 export IMG="/tmp/cheat-sheet.png"
 
 TEXT=`cat ${TEXT_FILE}`
-convert -background black -fill green -font Courier -pointsize 22 \
-  label:"${TEXT}" $IMG
+convert -size 1024x768 -background black -fill green \
+  -font Courier label:"${TEXT}" -trim -bordercolor black -border 50 +repage $IMG
 
 if [ -f ${IMG} ]; then
   feh -x --no-menus --on-last-slide quit --title "cheat-sheet" $IMG
