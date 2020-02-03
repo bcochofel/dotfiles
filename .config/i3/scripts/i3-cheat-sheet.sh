cee27386c0b82954b1b72866793145d3d1a1a9a0
Bruno Cochofel <bruno.cochofel@gmail.com>
2020-02-03 14:54:55 +0000

    i3 cheat sheets binding and scripts

---
+++ .config/i3/scripts/i3-cheat-sheet.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# converts text file to image to use with feh
+# depends on: imagemagick + feh
+
+export TEXT_FILE="${HOME}/.config/i3/scripts/i3-cheat-sheet.txt"
+export IMG="/tmp/cheat-sheet.png"
+
+TEXT=`cat ${TEXT_FILE}`
+convert -background black -fill green -font Courier -pointsize 22 \
+  label:"${TEXT}" $IMG
+
+if [ -f ${IMG} ]; then
+  feh -x --no-menus --on-last-slide quit --title "cheat-sheet" $IMG
+  rm -f $IMG
+fi
+
