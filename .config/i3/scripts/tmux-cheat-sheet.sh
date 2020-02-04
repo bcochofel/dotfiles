eabe84defb717cd2dfc1c7759a978fbd51c25354
Bruno Cochofel <bruno.cochofel@gmail.com>
2020-02-04 12:37:40 +0000

    key bindings

---
+++ .config/i3/scripts/tmux-cheat-sheet.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# converts text file to image to use with feh
+# depends on: imagemagick + feh
+
+export TEXT_FILE="${HOME}/.config/i3/scripts/tmux-cheat-sheet.txt"
+export IMG="/tmp/cheat-sheet.png"
+
+TEXT=`cat ${TEXT_FILE}`
+convert -size 1024x768 -background black -fill green \
+  -font Courier label:"${TEXT}" -trim -bordercolor black -border 50 +repage $IMG
+
+if [ -f ${IMG} ]; then
+  feh -x --no-menus --on-last-slide quit --title "cheat-sheet" $IMG
+  rm -f $IMG
+fi
+
