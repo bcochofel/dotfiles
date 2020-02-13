b82e2eb438c9ae1298df5fe46b8c3b662d95c976
Bruno Cochofel <bruno.cochofel@gmail.com>
2020-02-13 20:03:45 +0000

    i3 tray icons

--- bin/monitors.sh
+++ bin/monitors.sh
@@ -8,9 +8,11 @@ if [ $MON -gt 1 ]; then
     sleep 1
 #    xrandr | grep 'DP-2 connected' && xrandr --output eDP-1 --auto --output DP-2 --auto --above eDP-1 || xrandr --output DP-2 --off
     xrandr | grep 'DP-2 connected' && xrandr --output eDP-1-1 --scale 1x1 --output DP-2 --scale 1x1 --above eDP-1-1 || xrandr --output DP-2 --off
+    xrandr --output eDP-1-1 --primary
 else
     echo "One Monitor connected"
     xrandr --output eDP-1-1 --scale 1x1 --brightness 0.7
+    xrandr --output eDP-1-1 --primary
 fi
 
 exit 0
