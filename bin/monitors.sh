dce3fbf3bcb049354a5917dbd1d06814b44c5cb0
Bruno Cochofel <bruno.cochofel@gmail.com>
2020-03-08 14:59:35 +0000

    Monitors script

--- bin/monitors.sh
+++ bin/monitors.sh
@@ -8,14 +8,11 @@ if [ ${NR_MON} -gt 1 ]; then
     SECOND_MON=$(xrandr | grep -w connected | grep -v ${PRIMARY_MON} | awk '{ print $1 }')
     SECOND_MON_MODE="1920x1080"
     echo "More than one Monitor connected (${PRIMARY_MON} | ${SECOND_MON})"
-    xrandr --output ${PRIMARY_MON} --mode ${PRIMARY_MON_MODE} --scale 1x1 \
-      --output ${SECOND_MON} --mode ${SECOND_MON_MODE} --scale 1x1 \
-      --above ${PRIMARY_MON}
+    xrandr --output ${PRIMARY_MON} --mode ${PRIMARY_MON_MODE} --scale 1x1 --output ${SECOND_MON} --mode ${SECOND_MON_MODE} --scale 1x1 --above ${PRIMARY_MON}
     xrandr --output ${PRIMARY_MON} --primary
 else
   echo "One Monitor connected (${PRIMARY_MON})"
-    xrandr --output ${PRIMARY_MON} --mode ${PRIMARY_MON_MODE} --scale 1x1 \ 
-      --brightness 0.7
+    xrandr --output ${PRIMARY_MON} --mode ${PRIMARY_MON_MODE} --scale 1x1 --brightness 0.7
     xrandr --output ${PRIMARY_MON} --primary
 fi
 
