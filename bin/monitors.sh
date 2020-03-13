ee0fd8e68b8c87d87a30cada45441c74c4e7b53e
Bruno Cochofel <bruno.cochofel@gmail.com>
2020-03-13 13:52:02 +0000

    FIX for unplugged monitor

--- bin/monitors.sh
+++ bin/monitors.sh
@@ -11,8 +11,9 @@ xrandr --output HDMI-0 --off
 if [ ${NR_MON} -gt 1 ]; then
     SECOND_MON=$(xrandr | grep -w connected | grep -v ${PRIMARY_MON} | awk '{ print $1 }')
     SECOND_MON_MODE="1920x1080"
+    SECOND_MON_POS="--right-of"
     echo "More than one Monitor connected (${PRIMARY_MON} | ${SECOND_MON})"
-    xrandr --output ${PRIMARY_MON} --mode ${PRIMARY_MON_MODE} --scale 1x1 --output ${SECOND_MON} --mode ${SECOND_MON_MODE} --scale 1x1 --above ${PRIMARY_MON}
+    xrandr --output ${PRIMARY_MON} --mode ${PRIMARY_MON_MODE} --scale 1x1 --output ${SECOND_MON} --mode ${SECOND_MON_MODE} --scale 1x1 ${SECOND_MON_POS} ${PRIMARY_MON}
     xrandr --output ${PRIMARY_MON} --primary
 else
   echo "One Monitor connected (${PRIMARY_MON})"
