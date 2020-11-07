c24f7abca6c08ae773d07ec8f830d04629ddc5db
Bruno Cochofel <bruno.cochofel@gmail.com>
2020-11-07 17:25:45 +0000

    monitor at left

--- bin/monitors.sh
+++ bin/monitors.sh
@@ -12,7 +12,7 @@ if [ ${NR_MON} -gt 1 ]; then
     SECOND_MON=$(xrandr | grep -w connected | grep -v ${FIRST_MON} | awk '{ print $1 }')
     #SECOND_MON_MODE="1920x1080"
     SECOND_MON_MODE="2560x1440"
-    SECOND_MON_POS="--right-of"
+    SECOND_MON_POS="--left-of"
     echo "More than one Monitor connected (${FIRST_MON} | ${SECOND_MON})"
     xrandr --output ${FIRST_MON} --mode ${FIRST_MON_MODE} --scale 1x1 --output ${SECOND_MON} --mode ${SECOND_MON_MODE} --scale 1x1 ${SECOND_MON_POS} ${FIRST_MON}
     xrandr --output ${SECOND_MON} --primary
