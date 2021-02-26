413c0577badf1982c02980630aa32131193ab688
Bruno Cochofel <bruno.cochofel@gmail.com>
2021-02-26 08:41:20 +0000

    monitor to the right

--- bin/monitors.sh
+++ bin/monitors.sh
@@ -12,7 +12,7 @@ xrandr --output HDMI-1-1 --off
 if [ ${NR_MON} -gt 1 ]; then
     SECOND_MON=$(xrandr | grep -w connected | grep -v "${FIRST_MON} " | awk '{ print $1 }')
     SECOND_MON_MODE="2560x1440"
-    SECOND_MON_POS="--left-of"
+    SECOND_MON_POS="--right-of"
     echo "More than one Monitor connected (${FIRST_MON} | ${SECOND_MON})"
     xrandr --output ${FIRST_MON} --mode ${FIRST_MON_MODE} --scale 1x1 --output ${SECOND_MON} --mode ${SECOND_MON_MODE} --scale 1x1 ${SECOND_MON_POS} ${FIRST_MON}
     xrandr --output ${SECOND_MON} --primary
